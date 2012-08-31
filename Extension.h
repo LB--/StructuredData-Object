3b969c231860f9f0268725fa9b5f79ab9eebf08d
LB <LB@LB-Stuff.com>
2012-08-30 22:53:07 -0500

    Cleanup & version 2 started

--- Extension.h
+++ Extension.h
@@ -1,49 +1,29 @@
-#include <typeinfo>
 #include "StructuredData.hpp"
 
 class Extension
 {
 public:
-
 	LPRDATA rdPtr;
 	LPRH	rhPtr;
 
 	Edif::Runtime Runtime;
 
 	static const int MinimumBuild = 255;
-	static const int Version = 1;
+	static const int Version = 2;
 
 	static const int OEFLAGS = OEFLAG_VALUES|OEFLAG_RUNBEFOREFADEIN|OEFLAG_NEVERKILL;
 	static const int OEPREFS = 0;
 	
-	static const int WindowProcPriority = 100;
+	static const int WindowProcPriority = 0;
 
 	Extension(LPRDATA rdPtr, LPEDATA edPtr, fpcob cobPtr);
 	~Extension();
 
 
-	/*  Add any data you want to store in your extension to this class
-		(eg. what you'd normally store in rdPtr)
-
-		Unlike rdPtr, you can store real C++ objects with constructors
-		and destructors, without having to call them manually or store
-		a pointer.
-	*/
-
-	// int MyVariable;
-
 	SDNode *root, *cur;
 
 
-	/*  Add your actions, conditions and expressions as real class member
-		functions here. The arguments (and return type for expressions) must
-		match EXACTLY what you defined in the JSON.
-
-		Remember to link the actions, conditions and expressions to their
-		numeric IDs in the class constructor (Extension.cpp)
-	*/
-
-	/// Actions
+	//Actions
 	void ReturnToRoot();
 	void EnterSubnode(const char *Name);
 	void GotoNodeByID(int ID);
@@ -57,34 +37,24 @@ public:
 	void RemoveFloat(const char *Name);
 	void RemoveString(const char *Name);
 
-	/// Conditions
+	//Conditions
 	bool NodeExists(const char *Name);
 	bool NodeExistsByID(int ID);
 	bool IntExists(const char *Name);
 	bool FloatExists(const char *Name);
 	bool StringExists(const char *Name);
 
-	/// Expressions
+	//Expressions
 	int NodeID();
 	int Int(const char *Name);
 	float Float(const char *Name);
 	const char *String(const char *Name);
 
 
-
-	/* These are called if there's no function linked to an ID */
-
 	void Action(int ID, LPRDATA rdPtr, long param1, long param2);
 	long Condition(int ID, LPRDATA rdPtr, long param1, long param2);
 	long Expression(int ID, LPRDATA rdPtr, long param);
 
-
-
-
-	/*  These replace the functions like HandleRunObject that used to be
-		implemented in Runtime.cpp. They work exactly the same, but they're
-		inside the extension class.
-	*/
 
 	short Handle();
 	short Display();
@@ -94,5 +64,4 @@ public:
 
 	bool Save(HANDLE File);
 	bool Load(HANDLE File);
-
 };
